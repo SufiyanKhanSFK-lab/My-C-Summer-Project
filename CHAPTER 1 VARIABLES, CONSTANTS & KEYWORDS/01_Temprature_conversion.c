d3c7a71d0d27c6ea7d577c9a0fa9fcfe74f118cd
SufiyanKhanSFK-lab <theadventureoftotto@gmail.com>
2026-05-31 20:44:17 +0530

    Update 01_Temprature_conversion.c

--- CHAPTER 1 VARIABLES, CONSTANTS & KEYWORDS/01_Temprature_conversion.c
+++ CHAPTER 1 VARIABLES, CONSTANTS & KEYWORDS/01_Temprature_conversion.c
@@ -5,6 +5,8 @@ int main(){
     printf ("the temperature in celsius is: \n");
     scanf ("%f", &c);
     printf ("the temperature in fahrenheit is:%f\n", 1.8*c+32);
+    //in float calculation if we take no in fraction form then make sure that they are also written in float form, not in int
+    // as if we took (9/5)*c +32 then it gives wrong answer for that we have to take (9.0/5.0)*c + 32
     return 0;
 
 }
